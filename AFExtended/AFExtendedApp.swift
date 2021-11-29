4e348eb32388a9a5dfad19c20d473d0754d8c679
Zohra18 <ach.zohra@gmail.com>
2021-11-29 13:27:35 +0100

    Initial Commit

---
+++ AFExtended/AFExtendedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFExtendedApp.swift
+//  AFExtended
+//
+//  Created by Zohra Achour on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct AFExtendedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
