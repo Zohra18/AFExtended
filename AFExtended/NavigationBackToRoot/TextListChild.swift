ec306a820ab53d7d084116de2a8df96077a5e424
Zohra18 <ach.zohra@gmail.com>
2021-12-09 15:42:07 +0100

    Re Orga + comments on the back to root Navigation flow

---
+++ AFExtended/NavigationBackToRoot/TextListChild.swift
@@ -0,0 +1,24 @@
+//
+//  TextListChild.swift
+//  AFExtended
+//
+//  Created by Zohra Achour on 09/12/2021.
+//
+
+import SwiftUI
+
+struct TextListChild: View {
+    @Binding var backToRoot: Bool
+    var number: Int
+    var body: some View {
+        VStack {
+            Text("\(number)")
+            Button(action: {
+                backToRoot.toggle()
+            }, label: {
+               Text("back to root")
+            })
+        }
+        
+    }
+}
--- AFExtended/NavigationBackToRoot/TextListRoot.swift
+++ AFExtended/NavigationBackToRoot/TextListRoot.swift
@@ -7,57 +7,33 @@
 
 import SwiftUI
 
+//CONTENTE:
+//Détourner le bug existant avec le ForEach qui n'arrive pas à faire correspondre
+//les éléments de du tableau avec la View de destination
+//On passe donc par une var global pour la faire correspondre avec l'élément cliquer dans le ForEach
 struct TextListRoot: View {
     @State private var root = false
-    @State private var globalInt: Int? //<- optionnel parce que aucune valeur
+    @State private var globalInt: Int?
     var body: some View {
         NavigationView {
             VStack {
                 ForEach(0...5, id: \.self) { number in
-                    
-                
                         Text("\(number)")
                             .onTapGesture {
                                 globalInt = number
                                 root.toggle()
                             }
-                      
-                    
-                   
                 }
                 //déballe avec if let
                 if let unwrappedGlobalInt = globalInt {
-                    
                     NavigationLink(destination: TextListChild(backToRoot: $root, number: unwrappedGlobalInt), isActive: $root) {
                         EmptyView()
                     }
-                    
                 }
-                
             }
         }
     }
 }
-
-
-struct TextListChild: View {
-    @Binding var backToRoot: Bool
-    var number: Int
-    var body: some View {
-        VStack {
-            Text("\(number)")
-            Button(action: {
-                backToRoot.toggle()
-            }, label: {
-               Text("back to root")
-            })
-        }
-        
-    }
-}
-
-
-
 
 struct TextListRoot_Previews: PreviewProvider {
     static var previews: some View {
